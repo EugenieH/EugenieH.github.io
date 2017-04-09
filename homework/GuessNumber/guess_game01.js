5937639adbaf1ccab1a2de36855399cd210662bf
EugenieH <happyla424@gmail.com>
2017-04-09 15:28:52 +0800

    修改終極密碼bug

--- homework/GuessNumber/guess_game01.js
+++ homework/GuessNumber/guess_game01.js
@@ -1,4 +1,4 @@
-var n = Math.floor(Math.random()*100)+1;  //要猜的答案，random()隨機亂數的值在0.0～0.999之間
+var n = Math.floor(Math.random() * 100) + 1;  //要猜的答案，random()隨機亂數的值在0.0～0.999之間
 var num = 0;   //輸入的數字
 var minNum = 1;
 var maxNum = 100;
@@ -6,26 +6,26 @@ var maxNum = 100;
 num = prompt("請輸入數字:", "");
 
 if (num == null || num == n)  //剛開始將直接猜對或是未輸入者做判斷處理
-    if(num==null) {
-        document.write("你沒有輸入，答案是"+ n +".");
+    if (num == null) {
+        document.write("你沒有輸入，答案是" + n + ".");
     } else {
-        document.write("答對了，"+ n +".");
+        document.write("答對了，" + n + ".");
     }
-    for (var i=1; i<=num; i++)  {  //計算次數，並迴圈執行判斷
-        if(num>n) {
-            maxNum = num;
-            num = prompt("你輸入"+ num +"太高了，請再輸入:(" + minNum + "-" + maxNum + ")之間", "");
-    continue;
-    } else if (num<n) {
-            minNum = num;
-            num = prompt("你輸入"+ num +"太低了，請再輸入:(" + minNum + "-" + maxNum + ")之間", "");
-    continue;
+for (var i = 1; i <= num; i++) {  //計算次數，並迴圈執行判斷
+    if (num > n) {
+        maxNum = num;
+        num = prompt("你輸入" + num + "太高了，請再輸入:(" + minNum + "-" + maxNum + ")之間", "");
+        continue;
+    } else if (num < n) {
+        minNum = num;
+        num = prompt("你輸入" + num + "太低了，請再輸入:(" + minNum + "-" + maxNum + ")之間", "");
+        continue;
     } else if (num == n) {
-        document.write("答對了，"+ n +".<br />");
+        document.write("答對了，" + n + ".<br />");
         break;
     } else {
-        document.write("你放棄了，答案是"+ n +".");
+        document.write("你放棄了，答案是" + n + ".");
         break;
     }
 }
-document.write("恭喜你，共猜"+ i +"次"); //跳出迴圈並執行計算次數的工作
+document.write("恭喜你，共猜" + i + "次"); //跳出迴圈並執行計算次數的工作
